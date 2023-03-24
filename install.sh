9abd620d845a27177589ea9f386878ad76e3fde4
Marskey <marskeymill@icloud.com>
2023-03-24 10:44:26 +0800

    update

--- install.sh
+++ install.sh
@@ -1,12 +1,36 @@
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh  | sed 's/env\ zsh\ -l//')"
+#!/bin/bash
+git init --bare $HOME/.dotfiles
 wait
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+dotfiles="/usr/bin/git --git-dir=${HOME}/.dotfiles/ --work-tree=${HOME}"
+$dotfiles remote add origin https://github.com/Marskey/dotfiles.git
 wait
-git clone --recursive https://github.com/Marskey/.vim.git ~/.vim
+$dotfiles fetch
 wait
+$dotfiles checkout -b main --track origin/main
+wait
+$dotfiles submodule init
+wait
+$dotfiles submodule update
+wait
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh  | sed 's/env\ zsh\ -l//')"
+wait
+git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+wait
+if [[ $OSTYPE == 'darwin'* ]]; then
+  echo 'macOS'
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  wait
+  brew install --cask iterm2
+  wait
+  brew install nvim
+  wait
+  brew install tmux
+  wait
+elif [[ $OSTYPE == 'linux'* ]]; then
+  echo 'linux'
+  echo 'install nvim by youself'
+fi
 
-cp .zshrc ~/.zshrc
-cp .gitconfig ~/.
+ln -s $HOME/.config/nvim_nvchad_conf/ $HOME/.config/nvim/lua/custom
 
-# install vim vundle plugins
-vim -c 'PluginInstall' -c 'qa!'
+echo done!
