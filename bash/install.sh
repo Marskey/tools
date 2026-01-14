07a6a522566a7a00b4bd9336d74b115fe8eef75a
Marskey <marskeymill@icloud.com>
2026-01-14 17:11:59 +0800

    update

--- bash/install.sh
+++ bash/install.sh
@@ -2,84 +2,82 @@
 
 # 检测系统类型
 if [ -f /etc/fedora-release ]; then
-  PKG_MANAGER="dnf"
+  PKG_MANAGER="sudo dnf"
 elif [[ $OSTYPE == 'darwin'* ]]; then
   PKG_MANAGER="brew"
 else
   PKG_MANAGER="brew"
 fi
 
-git init --bare $HOME/.dotfiles
-wait
 dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
-$dotfiles remote add origin https://github.com/Marskey/dotfiles.git
-wait
-$dotfiles fetch
-wait
-$dotfiles checkout -b main --track origin/main
-wait
-$dotfiles submodule init
-wait
-$dotfiles submodule update
-wait
-# Change git setup to only show tracked Files
-$dotfiles config --local status.showUntrackedFiles no
-wait
+if [ ! -d "$HOME/.dotfiles" ]; then
+  git init --bare $HOME/.dotfiles
+  $dotfiles remote add origin https://github.com/Marskey/dotfiles.git
+  $dotfiles fetch
+  $dotfiles checkout -b main --track origin/main
+  $dotfiles submodule init
+  $dotfiles submodule update
+  $dotfiles config --local status.showUntrackedFiles no
+else
+  echo "dotfiles already exists, skipping..."
+fi
 
 # Install oh-my-zsh about
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh  | sed 's/env\ zsh\ -l//')"
-wait
-git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-wait
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-wait
-curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
-wait
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sed 's/env\ zsh\ -l//')"
+else
+  echo "oh-my-zsh already exists, skipping..."
+fi
+
+if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ]; then
+  git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+fi
+
+if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" ]; then
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+fi
+
+command -v zoxide &> /dev/null || curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
 # oh-my-zsh done
 
 # 只在非 Fedora 系统上安装 Homebrew
-if [[ $PKG_MANAGER != "dnf" ]]; then
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  wait
+if [[ $PKG_MANAGER != "sudo dnf" ]]; then
+  if ! command -v brew &> /dev/null; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-  if [[ $OSTYPE == 'darwin'* ]]; then
-    echo 'macOS'
-    xcode-select –install
-    wait
-  elif [[ $OSTYPE == 'linux'* ]]; then
-    echo 'linux'
-    (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /root/.bashrc
+    if [[ $OSTYPE == 'darwin'* ]]; then
+      echo 'macOS'
+      xcode-select –install
+    elif [[ $OSTYPE == 'linux'* ]]; then
+      echo 'linux'
+      (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /root/.bashrc
+    fi
+  else
+    echo "Homebrew already installed, skipping..."
   fi
 fi
 
-$PKG_MANAGER install nvim
-wait
+command -v nvim &> /dev/null || $PKG_MANAGER install nvim
 
 # tmux about
-$PKG_MANAGER install tmux
-wait
-git clone https://github.com/Morantron/tmux-fingers $HOME/.config/tmux/plugins/tmux-fingers
-wait
+command -v tmux &> /dev/null || $PKG_MANAGER install tmux
+
+if [ ! -d "$HOME/.config/tmux/plugins/tmux-fingers" ]; then
+  git clone https://github.com/Morantron/tmux-fingers $HOME/.config/tmux/plugins/tmux-fingers
+fi
 # tmux done
 
 # brew install delta
-$PKG_MANAGER install diffr
-wait
-$PKG_MANAGER install rlwrap
-wait
-$PKG_MANAGER install ripgrep
-wait
-$PKG_MANAGER install lazygit
-wait
-$PKG_MANAGER install fzf
-wait
-$PKG_MANAGER install jq fd
-wait
-$PKG_MANAGER install python3
-wait
-python3 -m pip install neovim-remote
-wait
+command -v diffr &> /dev/null || $PKG_MANAGER install diffr
+command -v rlwrap &> /dev/null || $PKG_MANAGER install rlwrap
+command -v rg &> /dev/null || $PKG_MANAGER install ripgrep
+command -v lazygit &> /dev/null || $PKG_MANAGER install lazygit
+command -v fzf &> /dev/null || $PKG_MANAGER install fzf
+command -v jq &> /dev/null || $PKG_MANAGER install jq
+command -v fd &> /dev/null || $PKG_MANAGER install fd
+command -v python3 &> /dev/null || $PKG_MANAGER install python3
+python3 -c "import neovim" 2>/dev/null || python3 -m pip install neovim-remote
 
-ln -s $HOME/.config/nvim_nvchad_conf/ $HOME/.config/nvim/lua/custom
+[ -L "$HOME/.config/nvim/lua/custom" ] || ln -s $HOME/.config/nvim_nvchad_conf/ $HOME/.config/nvim/lua/custom
 
 echo done!
